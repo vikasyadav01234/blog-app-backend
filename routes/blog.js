f70a7333367b01afcca51a5ef986e02b227f92ad
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-07-16 18:31:20 +0530

    "Added router.post for creating comments and exported router in blog.js"

--- routes/blog.js
+++ routes/blog.js
@@ -2,4 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 ///Import Controller
-const {createComment} = require("../controlles/CommentController")
+const {createComment} = require("../controlles/CommentController");
+
+//Mapping Create
+router.post("/comments/create", createComment);
+
+//export 
+module.exports = router;
